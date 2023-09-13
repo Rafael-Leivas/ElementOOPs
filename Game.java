717879bbf2a4d8238bae34f603e2c319a2ef9062
rafael-leivas <rafaelblsteam@gmail.com>
2023-09-12 23:54:14 -0300

    iniciando a utilizar o método de atacar

--- Game.java
+++ Game.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+
+import personagnes.Personagem;
+
 import java.util.Random;
 
 public class Game {
@@ -20,26 +23,11 @@ public class Game {
                     System.out.printf("Escolha um dos quatro elementos ou digite 0 para sair, [1] - WaveNerd(Água), [2] - StoneDev(Terra), [3] - BurnCoder(Fogo), [4] - BreezeHacker(Ar): ");
                     elemento = sc.nextInt();
                     System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------");
-                    if(elemento == 1) {
-                        elementoEscolhido = "WaveNerd";
-                    }else if (elemento == 2) {
-                        elementoEscolhido = "StoneDev";
-                    }else if (elemento == 3) {
-                        elementoEscolhido = "BurnCoder";
-                    }else {
-                        elementoEscolhido = "BreezeHacker";
-                    }
                 }while(elemento < 0 || elemento > 4);
         
                 if (elemento != 0){
-                    Elemento agua = new Elemento("WaveNerd", 0, 0, 0, 0, 0);
-                    Elemento terra = new Elemento("StoneDev", 0, 0, 0, 0, 0);
-                    Elemento fogo = new Elemento("BurnCoder", 0, 0, 0, 0, 0);
-                    Elemento ar = new Elemento("BreezeHacker", 0, 0, 0, 0, 0);
-            
-                    System.out.println("Você escolheu: " + elementoEscolhido);
-            
-                    System.out.println("Teste: " + agua);
+                    Personagem agua = new Personagem("WaveNerd", 0, 0, 0, 0, 0);
+                    agua.atacar();
 
                 }
     
