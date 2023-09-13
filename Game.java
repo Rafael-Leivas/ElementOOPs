f0ea0a7a6c947ea0a4c979adb6d46ba960aa5f9e
rafael-leivas <rafaelblsteam@gmail.com>
2023-09-13 19:25:00 -0300

    Alterando a lógica dos Do e Whiles

--- Game.java
+++ Game.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-import personagnes.Personagem;
+import elementos.ELemento;
 
 import java.util.Random;
 
@@ -11,26 +11,33 @@ public class Game {
         
         int jogar;
         int elemento;
-        String elementoEscolhido;
+        int elementoEscolhido;
 
         do {
             System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------");
-            System.out.print("Se deseja iniciar o Jogo tecle 1, caso queira sair tecle 0: ");
+            System.out.print("Se deseja iniciar o Jogo tecle [1], caso queira sair tecle [0]: ");
             jogar = sc.nextInt(); 
             if (jogar != 0) {
                 do {
                     System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------");
                     System.out.printf("Escolha um dos quatro elementos ou digite 0 para sair, [1] - WaveNerd(Água), [2] - StoneDev(Terra), [3] - BurnCoder(Fogo), [4] - BreezeHacker(Ar): ");
                     elemento = sc.nextInt();
-                    System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------");
                 }while(elemento < 0 || elemento > 4);
         
                 if (elemento != 0){
-                    Personagem agua = new Personagem("WaveNerd", 0, 0, 0, 0, 0);
-                    agua.atacar();
-
+                    do {
+                        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------");
+                        System.out.print("Se deseja jogar o torneio tecle [1], caso queira sair tecle [0]: ");
+                        jogar = sc.nextInt(); 
+                        
+                        if (jogar == 1) {
+                            System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------");
+                            ELemento agua = new ELemento("WaveNerd");
+                            
+                            agua.atacar();
+                        }
+                    }while(jogar != 0);
                 }
-    
             }
         }while(jogar != 0 && jogar != 1);
     }
