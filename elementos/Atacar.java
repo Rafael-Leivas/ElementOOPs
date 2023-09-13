ae7a29e0c4abb5333a70ccc07e3ffb567a748e65
rafael-leivas <rafaelblsteam@gmail.com>
2023-09-13 19:23:38 -0300

    Criando a classe de atacar

---
+++ elementos/Atacar.java
@@ -0,0 +1,11 @@
+package elementos;
+
+public class Atacar extends ELemento {
+
+    public Atacar(String nome) {
+        super(nome);
+        //TODO Auto-generated constructor stub
+    }
+
+    
+}
