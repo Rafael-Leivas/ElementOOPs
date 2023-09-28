59694e4fa8ed69e0c1c22cae7a2d1f3bfbeb233b
rafael-leivas <rafaelblsteam@gmail.com>
2023-09-28 12:05:12 -0300

    Criando a classe menu e adicionando os dois métodos a serem chamaodos através do Polimorfismo.

--- Game.java
+++ Game.java
@@ -7,42 +7,44 @@ import elementos.Personagens.Terra;
 
 public class Game {
     public static void main (String[] args){
-        Scanner sc = new Scanner(System.in);
-        
-        int jogar;
-        int elemento;
 
-        do {
-            System.out.println("------------------------------------");
-            System.out.print("Se deseja iniciar o Jogo tecle [1], caso queira sair tecle [0]: ");
-            jogar = sc.nextInt(); 
-            if (jogar != 0) {
-                do {
-                    System.out.println("------------------------------------");
-                    System.out.printf("Escolha um dos quatro elementos ou digite 0 para sair, [1] - WaveNerd(Água), [2] - StoneDev(Terra), [3] - BurnCoder(Fogo), [4] - BreezeHacker(Ar): ");
-                    elemento = sc.nextInt();
-                }while(elemento < 0 || elemento > 4);
+        int jogar = Menu.iniciarGame();
+        if (jogar == 1){
+            int elemento = Menu.escolhaPersonagem();
+            System.out.printf("O número escolhido foi: %d.\n" ,elemento);
+        }
+        // System.out.println(elemento);
+        // do {
+        //     System.out.println("------------------------------------");
+        //     System.out.print("Se deseja iniciar o Jogo tecle [1], caso queira sair tecle [0]: ");
+        //     jogar = sc.nextInt(); 
+        //     if (jogar == 1) {
+        //         do {
+        //             System.out.println("------------------------------------");
+        //             System.out.printf("Escolha um dos quatro elementos ou digite 0 para sair, [1] - WaveNerd(Água), [2] - StoneDev(Terra), [3] - BurnCoder(Fogo), [4] - BreezeHacker(Ar): ");
+        //             elemento = sc.nextInt();
+        //         }while(elemento < 0 || elemento > 4);
         
-                if (elemento != 0){
-                    do {
-                        System.out.println("------------------------------------");
-                        System.out.print("Se deseja jogar o torneio tecle [1], caso queira sair tecle [0]: ");
-                        jogar = sc.nextInt(); 
+        //         if (elemento != 0){
+        //             do {
+        //                 System.out.println("------------------------------------");
+        //                 System.out.print("Se deseja jogar o torneio tecle [1], caso queira sair tecle [0]: ");
+        //                 jogar = sc.nextInt(); 
                         
-                        if (jogar == 1) {
-                            System.out.println("------------------------------------");
-                            Agua agua = new Agua();
-                            Ar ar = new Ar();
-                            Fogo fogo = new Fogo();
-                            Terra terra = new Terra();
-                            System.out.println(agua);
-                            System.out.println(ar);
-                            System.out.println(fogo);
-                            System.out.println(terra);
-                        }
-                    }while(jogar != 0);
-                }
-            }
-        }while(jogar != 0 && jogar != 1);
+        //                 if (jogar == 1) {
+        //                     System.out.println("------------------------------------");
+        //                     Agua agua = new Agua();
+        //                     Ar ar = new Ar();
+        //                     Fogo fogo = new Fogo();
+        //                     Terra terra = new Terra();
+        //                     System.out.println(agua);
+        //                     System.out.println(ar);
+        //                     System.out.println(fogo);
+        //                     System.out.println(terra);
+        //                 }
+        //             }while(jogar != 0);
+        //         }
+        //     }
+        // }while(jogar == 0 || jogar == 1);
     }
 }
---
+++ Menu.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Menu {
+    private static final Scanner sc = new Scanner(System.in);
+    
+    public static int iniciarGame() {
+        int jogar;
+        do {
+            System.out.println("------------------------------------");
+            System.out.print("---- Guerra de elementos ----\n[1] - Iniciar jogo;\n[0] - Sair\n");
+            while (!sc.hasNextInt()) {
+                sc.next(); // Limpar a entrada não válida
+                System.out.println("Digite um número válido (0 ou 1).");
+            }
+            jogar = sc.nextInt(); 
+            if (jogar == 0)
+                break;
+        } while(jogar != 0 && jogar != 1);
+        System.out.printf("O número escolhido foi: %d.\n" ,jogar);
+        return jogar;
+    }
+
+    public static int escolhaPersonagem() {
+        int elemento;
+        do {
+            System.out.println("------------------------------------");
+            System.out.printf("Escolha um elemento\n [1] - WaveNerd(Água);\n [2] - StoneDev(Terra);\n [3] - BurnCoder(Fogo);\n [4] - BreezeHacker(Ar);\n [0] - Sair;\n");
+            while (!sc.hasNextInt()) {
+                sc.next(); // Limpar a entrada não válida
+                System.out.println("Digite um número válido (0 a 4).");
+            }
+            elemento = sc.nextInt();
+        } while(elemento > 0 || elemento < 4);
+        return elemento;
+    }
+
+}
