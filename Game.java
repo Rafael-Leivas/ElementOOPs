249c0c8819c4379dad699470a7eed848a15bde35
rafael-leivas <rafaelblsteam@gmail.com>
2023-10-17 19:57:32 -0300

    .

--- Game.java
+++ Game.java
@@ -3,8 +3,10 @@ public class Game {
 
         int jogar = Menu.iniciarGame();
         if (jogar == 1){
-            int elemento = Menu.escolhaPersonagem();
+            int elemento = Menu.iniciarProximaBatalha();
             System.out.printf("O número escolhido foi: %d.\n" ,elemento);
+        } else {
+            System.out.println("Volte sempre. s2");
         }
     }
 }
