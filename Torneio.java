388e50d90f8de2a132293ca8a6c453ac58f8ed6d
rafael-leivas <rafaelblsteam@gmail.com>
2023-10-21 15:26:45 -0300

    Separando em métodos no torneio

--- Torneio.java
+++ Torneio.java
@@ -1,26 +1,22 @@
 public class Torneio {
 
-    public static void realizarBatalha(Elemento personagem1, Elemento personagem2) {
+    public static void realizarBatalha(Elemento meuPersonagem, Elemento oponente) {
 
-        int velocidade1 = personagem1.getVelocidade();
-        int velocidade2 = personagem2.getVelocidade();
+        int velocidade1 = meuPersonagem.getVelocidade();
+        int velocidade2 = oponente.getVelocidade();
         
         if (velocidade1 > velocidade2) {
             System.out.println("-------------------------------------------------------");
             System.out.println("VOCÊ CONTÉM A VELOCIDADE MAIOR E ATACA PRIMEIRO");
             
-            while (personagem1.getVida() > 0 && personagem2.getVida() > 0) {
+            while (meuPersonagem.getVida() > 0 && oponente.getVida() > 0) {
                 int escolherAtaque = Menu.atacar();
                 if (escolherAtaque == 1) {
-                    int danoCausado = personagem1.atacar(personagem2.getDefesa());
-                    diminuirVida(personagem2, danoCausado);
-    
-                    System.out.println("DANO CAUSADO");
-                    System.out.println("Ataque do " + personagem1.getNome() + ": " + danoCausado);
-                    System.out.println("Vida do " + personagem2.getNome() + ": " + personagem2.getVida());
-                    System.out.println("-------------------------------------------------------");
+                    ataque(meuPersonagem, oponente);
                 } else if (escolherAtaque == 2) {
                     // Implemente a lógica do ataque elemental
+                } else {
+                    break;
                 }
             }
         } else if (velocidade1 < velocidade2) {
@@ -36,6 +32,16 @@ public class Torneio {
         int vida = personagem.getVida();
         vida -= danoCausado;
         personagem.setVida(vida);
+    }
+
+    public static void ataque(Elemento personagem1, Elemento personagem2) {
+        int danoCausado = personagem1.atacar(personagem2.getDefesa());
+            diminuirVida(personagem2, danoCausado);
+
+            System.out.println("DANO CAUSADO");
+            System.out.println("Ataque do " + personagem1.getNome() + ": " + danoCausado);
+            System.out.println("Vida do " + personagem2.getNome() + ": " + personagem2.getVida());
+            System.out.println("-------------------------------------------------------");
     }
 
 }
